8afd854af283d1f08001700c92a87dc4820da7fc
Natali501 <n73882108@gmail.com>
2025-05-31 19:49:30 +0300

    задание 1,2

---
+++ homework-5/index.js
@@ -0,0 +1,13 @@
+//'Java Script-код с созданием переменных'
+/*'автор'
+Евладенко Наталья*/
+let myName='Наталья';
+console.log(myName);
+let mySurname='Евладенко';
+console.log(mySurname);
+let myFavoriteDrink='коффе';
+console.log(myFavoriteDrink);
+let myFavoriteAnimel='кошка';
+console.log(myFavoriteAnimel);
+const myFavoriteProgrammingLanguage='JavaScript';
+console.log(myFavoriteProgrammingLanguage);
